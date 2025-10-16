048939ec69cc29b8670f7a4a9f0863cddc340ef1
Matheus-C-Machado <matheusmonster08@gmail.com>
2025-10-16 19:09:45 -0300

    ajuste

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'order',
     'product',	
     'rest_framework.authtoken',
+    'rest_framework',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,17 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    
+}
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
